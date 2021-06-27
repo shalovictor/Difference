47f3fd97e460b75c53a004b47e9db867d86c5843
shalovictor <86366745+shalovictor@users.noreply.github.com>
2021-06-27 16:15:47 +0530

    Create random.java

---
+++ random.java
@@ -0,0 +1,10 @@
+public class random
+{
+  public static void main(String args[])
+  {
+    double d;
+    System.out.println("Ramdom Number between 0 and 1 :");
+    d = Math.random();
+    System.out.println(d);
+  }
+}
