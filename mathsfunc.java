c65bdd4741c7b0a904bf7fe823d10f6d98930ffb
shalovictor <86366745+shalovictor@users.noreply.github.com>
2021-06-27 16:59:36 +0530

    Create mathsfunc.java

---
+++ mathsfunc.java
@@ -0,0 +1,22 @@
+public class mathfunc
+{
+  public static void main(String args[])
+  {
+    double m=6.25, n=8.75, p=15.625;
+    System.out.println("The output of different mathemtical functions are :");
+    System.out.println(" Minimum value between 6.25 & 8.75= " +Math.min(m,n));
+    System.out.println(" Maximum value between 6.25 & 8.75= " +Math.max(m,n));
+    System.out.println("Square root of 6.25 =" +Math.sqrt(m));
+    System.out.println("Cube root of 6.25 =" +Math.cbrt(p));
+    System.out.println("6.25 raised to the power 3 =" +Math.pow(m,3));
+    System.out.println("Absolute value of (6.25-8.75) =" +Math.abs(m-n));
+    System.out.println("The floor value of 6.25 =" +Math.floor(m));
+    System.out.println("The ceil value of 6.25 =" +Math.ceil(m));
+    System.out.println("The integral value of 6.25 =" +Math.rint(m));
+    System.out.println("The rounded value of 6.25 =" +Math.round(m));
+    System.out.println("The exponent value of 6.25 =" +Math.exp(m));
+    System.out.println("The natural log value of 6.25 =" +Math.log(m));
+  }
+}
+    
+                    
