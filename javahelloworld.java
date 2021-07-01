f857716ecfb634d10d78ab2b8e12bce236e22f55
shalovictor <86366745+shalovictor@users.noreply.github.com>
2021-07-01 19:58:20 +0530

    Create javahelloworld.java

---
+++ javahelloworld.java
@@ -0,0 +1,9 @@
+public class JavaHelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Before calling System.exit()");
+        System.exit(0);
+        System.out.println("After calling System.exit()");
+    }
+}
